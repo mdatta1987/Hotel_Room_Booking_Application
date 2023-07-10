e9d1fbf0d061089924ae395f7860d53248ad6427
mdatta1987 <welcometofairyland@gmail.com>
2023-07-10 11:42:23 +0530

    Discussion_Forum

---
+++ Discussion_Forum/src/discussion/forum/units/service/QuestionService.java
@@ -0,0 +1,13 @@
+package discussion.forum.units.service;
+
+import com.forum.units.Question;
+import com.forum.units.User;
+
+public interface QuestionService
+{
+	Question createQuestion(String title, String message, User user);
+	
+	Question getQuestionById(long id);
+	
+	void deleteQuestion(Question question);
+}
---
+++ Discussion_Forum/src/discussion/forum/units/service/UserService.java
@@ -0,0 +1,11 @@
+package discussion.forum.units.service;
+
+import com.forum.units.User;
+import com.forum.units.UserRole;
+
+public interface UserService
+{
+	User createUser(String username, String password, String email, UserRole userRole);
+	
+	User getUser(String username, String password);
+}
